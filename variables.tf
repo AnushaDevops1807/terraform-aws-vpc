ab05a91654c4723a2c1848bb5bdcc7397ef721d9
Anusha N <nanu18071989@gmail.com>
2025-02-05 23:48:41 +0800

    terraform-aws-vpc

--- variables.tf
+++ variables.tf
@@ -80,10 +80,10 @@ variable "database_route_table_tags" {
     default = {}
 }
 
-/* variable "is_peering_required" {
+variable "is_peering_required" {
     default = false
 }
 
 variable "vpc_peering_tags" {
     default = {}
-} */
+}
